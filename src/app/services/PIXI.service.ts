4a24573c3aa0a10e89ebeb48abdf3c69cbe533c2
muxi2015 <lin912305043@gmail.com>
2018-06-23 15:54:44 +0800

    Drag & Drop Is Work

--- src/app/services/PIXI.service.ts
+++ src/app/services/PIXI.service.ts
@@ -15,29 +15,40 @@ import {
 } from 'gsap'
 import 'pixi-layers'
 import { Linear } from 'gsap'
-import { debounceTime } from 'rxjs/operators';
-import { EventTargetLike } from 'rxjs/src/observable/FromEventObservable';
-import { Observable } from 'rxjs/Observable';
+import { debounceTime } from 'rxjs/operators'
+import { EventTargetLike } from 'rxjs/src/observable/FromEventObservable'
+import { Observable } from 'rxjs/Observable'
+import { both } from 'ramda'
+import {
+  onDragEnd,
+  onDragMove,
+  onDragStart,
+  createText,
+} from '../pixi'
 
 @Injectable()
 export class PIXIService {
-  public app: PIXI.Application
-  private _renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer
-  private _origin: any
-  private _view: HTMLCanvasElement
-  private _topLayer: PIXI.display.Layer
-  private _middleLayer: PIXI.display.Layer
-  private _bottomLayer: PIXI.display.Layer
-  private _stage: PIXI.Container
-  private _layer: PIXI.display.Layer
-  private _container: PIXI.Container;
+  private app: PIXI.Application
+  private view: HTMLCanvasElement
+  private stage: PIXI.Container
+  private renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer
 
   public initialize(config: PIXIConfig) {
-    const animate = () => {
+    const render = () => {
+      this.renderer.render(this.stage)
+      requestAnimationFrame(render)
+    }
 
-      // fpsText.text = 'FPS:' + String(this.app.ticker.FPS.toFixed(1))
-      this._renderer.render(this._container)
-      requestAnimationFrame(animate)
+    const subscribe = (obj) => {
+      obj.interactive = true
+      obj.on('mousedown', onDragStart)
+        .on('touchstart', onDragStart)
+        .on('mouseup', onDragEnd)
+        .on('mouseupoutside', onDragEnd)
+        .on('touchend', onDragEnd)
+        .on('touchendoutside', onDragEnd)
+        .on('mousemove', onDragMove)
+        .on('touchmove', onDragMove)
     }
 
     this.app = new PIXI.Application({
@@ -50,201 +61,219 @@ export class PIXIService {
       view: document.querySelector('#canvas'),
     })
 
-    this._stage = this.app.stage
-    this._view = this.app.view
-    this._renderer = this.app.renderer
+    this.app.stage = new PIXI.display.Stage()
+    this.app.stage.group.enableSort = true
 
-    this._container = new PIXI.Container()
-    this._topLayer = new PIXI.display.Layer()
+    this.view = this.app.view
+    this.stage = this.app.stage
+    this.renderer = this.app.renderer
 
-    this._stage.addChild(this._container)
-    this._stage.addChild(this._topLayer)
-
+    this.app.renderer.autoResize = true
     Observable.fromEvent(window, 'resize')
       .pipe(debounceTime(200))
       .subscribe((e: EventTarget | EventTargetLike | any) => {
-        this.app.renderer.resize(window.innerWidth, window.innerHeight);
+        let w
+        let h
+        if (window.innerWidth / window.innerHeight >= config.ratio) {
+          w = window.innerHeight * config.ratio
+          h = window.innerHeight
+        } else {
+          w = window.innerWidth
+          h = window.innerWidth / config.ratio
+        }
+        this.renderer.view.style.width = w + 'px'
+        this.renderer.view.style.height = h + 'px'
       })
 
-    const fpsText = this.initFPS()
-    // this.initTitle()
-    // this.initMenu()
+    const backgroundAnimationGroup = new PIXI.display.Group(1, false)
 
+    const dragGroup = new PIXI.display.Group(2, false)
 
-    // this._topLayer = new PIXI.display.Layer()
-    // this._middleLayer = new PIXI.display.Layer()
-    // this._bottomLayer = new PIXI.display.Layer()
-
-    // this._renderer.render(this._stage)
-
-    // this.app.stage.addChild(this._topLayer)
-    // this.app.stage.addChild(this._middleLayer)
-    // this.app.stage.addChild(this._bottomLayer)
-
-    animate()
-    // this.handleConfig(config)
-
-  }
-
-  private initMenu() {
-    const offset = 40
-    const star = this.drawStar(8)
-
-    const showStage = new PIXI.display.Layer()
-
-    showStage.position.set(this._view.width - 80, offset)
-    showStage.addChild(star)
-    this._stage.addChild(showStage)
-
-  }
-
-  private initTitle() {
-    let lastRectX = 0
-    const generatorRect = (s: string, index: number) => {
-      const text = new PIXI.Text(s, style)
-      text.style.dropShadow = true
-
-      const rect = new PIXI.Graphics()
-      rect.lineStyle(5, Colors.white, 1)
-      rect.addChild(text)
-
-      const random = Math.round(Math.random() * 5 + 4)
-      const value = Math.round(Math.random()) === 1 ? 0.2 : -0.2
-
-      const randomRotation = parseFloat((Math.random() * value).toFixed(9))
-
-      const width = random * 8
-      const height = 5 * 8
-      text.style.fontSize = random * 5
-      rect.position.x += lastRectX
-      lastRectX += width - 10
-      rect.rotation = randomRotation
-      text.position.x = rect.width / 2.5
-
-      return {
-        red: (): PIXI.Graphics => {
-          text.style.fill = Colors.white
-          rect.beginFill(Colors.red)
-          rect.drawRect(0, 0, width, height)
-          rect.endFill()
-          return rect
-        },
-        black: (): PIXI.Graphics => {
-          text.style.fill = Colors.red
-          rect.beginFill(Colors.black)
-          rect.drawRect(0, 0, width, height)
-          rect.endFill()
-          return rect
-        },
-      }
-    }
-
-    const style: TextStyleOptions = {
-      fontFamily: ['persona', 'Arial'],
-      align: 'center',
-      fontStyle: 'italic',
-      fontWeight: 'bold',
-    }
-
-    const texts = 'BiliBili 趋 势'.replace(/[ ]/g, '').split('')
-
-    const textContainer = new PIXI.display.Layer()
-
-    texts.forEach((s: string, i: number) => {
-      const rect = Math.round(Math.random()) === 0
-        ? generatorRect(s, i).red()
-        : generatorRect(s, i).black()
-      textContainer.addChild(rect)
+    const greenGroup = new PIXI.display.Group(0, true)
+    greenGroup.on('sort', function (sprite) {
+      // green bunnies go down
+      sprite.zOrder = -sprite.y
     })
 
-    // 居中
-    textContainer.x = texts.length * 10
-    textContainer.y = 50
-    this._stage.addChild(textContainer)
-  }
+    this.stage.addChild(new PIXI.display.Layer(backgroundAnimationGroup))
 
-  private drawStar(count = 5): PIXI.Graphics {
-    const offset = 40
-    const graphics = new PIXI.Graphics()
+    this.stage.addChild(new PIXI.display.Layer(greenGroup))
+    this.stage.addChild(new PIXI.display.Layer(dragGroup))
 
-    range(count + 1, 1, -1).map((v) => {
-      const color = v % 2 === 0 ? Colors.white : Colors.black
-      graphics.beginFill(color)
-      graphics.drawStar(offset, offset,
-        5, v * 4, count * 2)
-      graphics.endFill()
-    })
+    const textureGreen = PIXI.Texture.fromImage('assets/img/square_blue.png')
 
-    graphics.interactive = true
-    graphics.cursor = 'pointer'
-    return graphics
-  }
+    const bunniesOdd = new PIXI.Container()
+    const bunniesEven = new PIXI.Container()
 
-  private addStarAnimation(target: PIXI.Container) {
-    const x = target.x - 15
-    const y = target.y - 10
-    const width = target.width
-    const height = target.height
-    const star = this.drawStar(5)
-    star.scale.set(0.3, 0.3)
-    const startList = new TimelineLite()
+    const FPSContaniner = new PIXI.Container()
 
-    startList.addLabel('moving', 5)
+    this.stage.addChild(bunniesOdd)
+    this.stage.addChild(bunniesEven)
+    this.stage.addChild(FPSContaniner)
 
-    const starContainer = new PIXI.display.Layer()
-    starContainer.position.set(x, y)
-    starContainer.height = height
-    starContainer.width = width
-    starContainer.addChild(star)
+    // for (let i = 0; i < 15; i++) {
+    //   const bunny = new PIXI.Sprite(textureGreen)
+    //   bunny.width = 50
+    //   bunny.height = 50
+    //   bunny.position.set(100 + 20 * i, 100 + 20 * i)
+    //   bunny.anchor.set(0.5)
+    //   // that thing is required
+    //   bunny.parentGroup = greenGroup
+    //   if (i % 2 === 0) {
+    //     bunniesEven.addChild(bunny)
+    //   } else {
+    //     bunniesOdd.addChild(bunny)
+    //   }
+    //   subscribe(bunny)
+    // }
 
-    // starContainer.parentGroup = this._bottomGroup
-    starContainer.zIndex = 1
-    starContainer.parentLayer = this._layer
+    const FPS = this.createFPS()
+    FPSContaniner.addChild(FPS)
+    FPS.parentGroup = dragGroup
+    subscribe(FPSContaniner)
 
-    this._container.addChild(starContainer)
-
-    TweenMax.to(starContainer.position, 1.5, {
-      x: x + width * 2,
-      repeat: -1,
-      ease: Linear.easeNone
-    })
-
-    startList.play('moving')
+    FPSContaniner.position.set(15, this.view.height - 35)
 
   }
 
-  private initFPS() {
-    const fpsText = new PIXI.Text('', {
-      fontFamily: ['persona', 'Arial'],
-      align: 'center',
-      fontWeight: 'bold',
-      fill: Colors.white,
-      dropShadow: true,
-    })
+  // private initMenu() {
+  //   const offset = 40
+  //   const star = this.drawStar(8)
+  //
+  //   const showStage = new PIXI.display.Layer()
+  //
+  //   showStage.position.set(this._view.width - 80, offset)
+  //   showStage.addChild(star)
+  //   this._stage.addChild(showStage)
+  //
+  // }
+  //
+  // private initTitle() {
+  //   let lastRectX = 0
+  //   const generatorRect = (s: string, index: number) => {
+  //     const text = new PIXI.Text(s, style)
+  //     text.style.dropShadow = true
+  //
+  //     const rect = new PIXI.Graphics()
+  //     rect.lineStyle(5, Colors.white, 1)
+  //     rect.addChild(text)
+  //
+  //     const random = Math.round(Math.random() * 5 + 4)
+  //     const value = Math.round(Math.random()) === 1 ? 0.2 : -0.2
+  //
+  //     const randomRotation = parseFloat((Math.random() * value).toFixed(9))
+  //
+  //     const width = random * 8
+  //     const height = 5 * 8
+  //     text.style.fontSize = random * 5
+  //     rect.position.x += lastRectX
+  //     lastRectX += width - 10
+  //     rect.rotation = randomRotation
+  //     text.position.x = rect.width / 2.5
+  //
+  //     return {
+  //       red: (): PIXI.Graphics => {
+  //         text.style.fill = Colors.white
+  //         rect.beginFill(Colors.red)
+  //         rect.drawRect(0, 0, width, height)
+  //         rect.endFill()
+  //         return rect
+  //       },
+  //       black: (): PIXI.Graphics => {
+  //         text.style.fill = Colors.red
+  //         rect.beginFill(Colors.black)
+  //         rect.drawRect(0, 0, width, height)
+  //         rect.endFill()
+  //         return rect
+  //       },
+  //     }
+  //   }
+  //
+  //   const style: TextStyleOptions = {
+  //     fontFamily: ['persona', 'Arial'],
+  //     align: 'center',
+  //     fontStyle: 'italic',
+  //     fontWeight: 'bold',
+  //   }
+  //
+  //   const texts = 'BiliBili 趋 势'.replace(/[ ]/g, '').split('')
+  //
+  //   const textContainer = new PIXI.display.Layer()
+  //
+  //   texts.forEach((s: string, i: number) => {
+  //     const rect = Math.round(Math.random()) === 0
+  //       ? generatorRect(s, i).red()
+  //       : generatorRect(s, i).black()
+  //     textContainer.addChild(rect)
+  //   })
+  //
+  //   // 居中
+  //   textContainer.x = texts.length * 10
+  //   textContainer.y = 50
+  //   this._stage.addChild(textContainer)
+  // }
+  //
+  // private drawStar(count = 5): PIXI.Graphics {
+  //   const offset = 40
+  //   const graphics = new PIXI.Graphics()
+  //
+  //   range(count + 1, 1, -1).map((v) => {
+  //     const color = v % 2 === 0 ? Colors.white : Colors.black
+  //     graphics.beginFill(color)
+  //     graphics.drawStar(offset, offset,
+  //       5, v * 4, count * 2)
+  //     graphics.endFill()
+  //   })
+  //
+  //   graphics.interactive = true
+  //   graphics.cursor = 'pointer'
+  //   return graphics
+  // }
+  //
+  // private addStarAnimation(target: PIXI.Container) {
+  //   const x = target.x - 15
+  //   const y = target.y - 10
+  //   const width = target.width
+  //   const height = target.height
+  //   const star = this.drawStar(5)
+  //   star.scale.set(0.3, 0.3)
+  //   const startList = new TimelineLite()
+  //
+  //   startList.addLabel('moving', 5)
+  //
+  //   const starContainer = new PIXI.display.Layer()
+  //   starContainer.position.set(x, y)
+  //   starContainer.height = height
+  //   starContainer.width = width
+  //   starContainer.addChild(star)
+  //
+  //   // starContainer.parentGroup = this._bottomGroup
+  //   starContainer.zIndex = 1
+  //   starContainer.parentLayer = this._layer
+  //
+  //   this._container.addChild(starContainer)
+  //
+  //   TweenMax.to(starContainer.position, 1.5, {
+  //     x: x + width * 2,
+  //     repeat: -1,
+  //     ease: Linear.easeNone
+  //   })
+  //
+  //   startList.play('moving')
+  //   const stars = new PIXI.display.Group(1, false)
+  //
+  //
+  //   return stars
+  // }
 
-    // 设置 倾斜
-    fpsText.skew.set(-0.5, 0)
-    const textContainer = new PIXI.display.Layer()
+  private createFPS() {
+    const FPS = createText("FPS:" + this.app.ticker.FPS.toFixed(2))
+    FPS.skew.set(-0.5, 0)
+    return FPS
+  }
 
-    textContainer.position.set(15, this._view.height - 30)
-
-    const textRect = new PIXI.Graphics()
-
-    textContainer.addChild(textRect)
-    textContainer.addChild(fpsText)
-
-    textRect.beginFill(Colors.red)
-    textRect.drawStar(textContainer.x, textContainer.y, 10, 10, 10)
-    textRect.endFill()
-
-    textContainer.zIndex = 10
-    textContainer.parentLayer = this._layer
-
-    this._container.addChild(textContainer)
-
-    this.addStarAnimation(textContainer)
-
-    return fpsText
+  private createFPSAnimation() {
+    
   }
 
 }
