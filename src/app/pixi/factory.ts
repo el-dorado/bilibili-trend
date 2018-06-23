1368a2ca025bf18cd0ff3abacd3950e41b984037
muxi2015 <lin912305043@gmail.com>
2018-06-23 23:28:02 +0800

    update doc

--- src/app/pixi/factory.ts
+++ src/app/pixi/factory.ts
@@ -2,10 +2,12 @@ import * as PIXI from 'pixi.js'
 import { Colors } from '../utils'
 
 /**
- * 构建文字
+ * 创建一个 PIXI.Text 对象
+ * @param text - 文字
+ * @param szie - 文字大小 默认为12
  * @return {PIXI.Text}
  */
-export function createText(text) {
+export function createText(text, szie = 12) {
   return new PIXI.Text(text, {
     fontFamily: ['persona', 'Arial'],
     align: 'center',
