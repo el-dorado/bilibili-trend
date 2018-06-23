3bd5395e000b35febcc4b02830fbafe331d198b9
muxi2015 <lin912305043@gmail.com>
2018-06-23 23:30:50 +0800

    Refactor Title

--- src/app/services/PIXI.service.ts
+++ src/app/services/PIXI.service.ts
@@ -89,50 +89,29 @@ export class PIXIService {
 
     const dragGroup = new PIXI.display.Group(2, false)
 
-    const greenGroup = new PIXI.display.Group(0, true)
-    greenGroup.on('sort', function (sprite) {
-      // green bunnies go down
-      sprite.zOrder = -sprite.y
-    })
-
     this.stage.addChild(new PIXI.display.Layer(backgroundAnimationGroup))
-
-    this.stage.addChild(new PIXI.display.Layer(greenGroup))
     this.stage.addChild(new PIXI.display.Layer(dragGroup))
 
-    const textureGreen = PIXI.Texture.fromImage('assets/img/square_blue.png')
-
-    const bunniesOdd = new PIXI.Container()
-    const bunniesEven = new PIXI.Container()
-
     const FPSContaniner = new PIXI.Container()
+    const TitleContaniner = new PIXI.Container()
 
-    this.stage.addChild(bunniesOdd)
-    this.stage.addChild(bunniesEven)
     this.stage.addChild(FPSContaniner)
-
-    // for (let i = 0; i < 15; i++) {
-    //   const bunny = new PIXI.Sprite(textureGreen)
-    //   bunny.width = 50
-    //   bunny.height = 50
-    //   bunny.position.set(100 + 20 * i, 100 + 20 * i)
-    //   bunny.anchor.set(0.5)
-    //   // that thing is required
-    //   bunny.parentGroup = greenGroup
-    //   if (i % 2 === 0) {
-    //     bunniesEven.addChild(bunny)
-    //   } else {
-    //     bunniesOdd.addChild(bunny)
-    //   }
-    //   subscribe(bunny)
-    // }
-
-    const FPS = this.createFPS()
-    FPSContaniner.addChild(FPS)
-    FPS.parentGroup = dragGroup
+    this.stage.addChild(TitleContaniner)
     subscribe(FPSContaniner)
 
+    const FPS = this.createFPS()
+    FPS.parentGroup = dragGroup
+    // this.createFPSAnimation().map((v) => FPSContaniner.addChild(v))
     FPSContaniner.position.set(15, this.view.height - 35)
+    FPSContaniner.addChild(FPS)
+
+    const texts = this.createTitle()
+    TitleContaniner.position.set(15, 50)
+    texts.map((v) => {
+      v.parentGroup = dragGroup
+      subscribe(v)
+      TitleContaniner.addChild(v)
+    })
 
   }
 
@@ -147,89 +126,65 @@ export class PIXIService {
   //   this._stage.addChild(showStage)
   //
   // }
-  //
-  // private initTitle() {
-  //   let lastRectX = 0
-  //   const generatorRect = (s: string, index: number) => {
-  //     const text = new PIXI.Text(s, style)
-  //     text.style.dropShadow = true
-  //
-  //     const rect = new PIXI.Graphics()
-  //     rect.lineStyle(5, Colors.white, 1)
-  //     rect.addChild(text)
-  //
-  //     const random = Math.round(Math.random() * 5 + 4)
-  //     const value = Math.round(Math.random()) === 1 ? 0.2 : -0.2
-  //
-  //     const randomRotation = parseFloat((Math.random() * value).toFixed(9))
-  //
-  //     const width = random * 8
-  //     const height = 5 * 8
-  //     text.style.fontSize = random * 5
-  //     rect.position.x += lastRectX
-  //     lastRectX += width - 10
-  //     rect.rotation = randomRotation
-  //     text.position.x = rect.width / 2.5
-  //
-  //     return {
-  //       red: (): PIXI.Graphics => {
-  //         text.style.fill = Colors.white
-  //         rect.beginFill(Colors.red)
-  //         rect.drawRect(0, 0, width, height)
-  //         rect.endFill()
-  //         return rect
-  //       },
-  //       black: (): PIXI.Graphics => {
-  //         text.style.fill = Colors.red
-  //         rect.beginFill(Colors.black)
-  //         rect.drawRect(0, 0, width, height)
-  //         rect.endFill()
-  //         return rect
-  //       },
-  //     }
-  //   }
-  //
-  //   const style: TextStyleOptions = {
-  //     fontFamily: ['persona', 'Arial'],
-  //     align: 'center',
-  //     fontStyle: 'italic',
-  //     fontWeight: 'bold',
-  //   }
-  //
-  //   const texts = 'BiliBili 趋 势'.replace(/[ ]/g, '').split('')
-  //
-  //   const textContainer = new PIXI.display.Layer()
-  //
-  //   texts.forEach((s: string, i: number) => {
-  //     const rect = Math.round(Math.random()) === 0
-  //       ? generatorRect(s, i).red()
-  //       : generatorRect(s, i).black()
-  //     textContainer.addChild(rect)
-  //   })
-  //
-  //   // 居中
-  //   textContainer.x = texts.length * 10
-  //   textContainer.y = 50
-  //   this._stage.addChild(textContainer)
-  // }
-  //
-  // private drawStar(count = 5): PIXI.Graphics {
-  //   const offset = 40
-  //   const graphics = new PIXI.Graphics()
-  //
-  //   range(count + 1, 1, -1).map((v) => {
-  //     const color = v % 2 === 0 ? Colors.white : Colors.black
-  //     graphics.beginFill(color)
-  //     graphics.drawStar(offset, offset,
-  //       5, v * 4, count * 2)
-  //     graphics.endFill()
-  //   })
-  //
-  //   graphics.interactive = true
-  //   graphics.cursor = 'pointer'
-  //   return graphics
-  // }
-  //
+
+  private createTitle() {
+    let lastRectX = 0
+    const style: TextStyleOptions = {
+      fontFamily: ['persona', 'Arial'],
+      align: 'center',
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+    }
+    const generatorRect = (s: string, index: number) => {
+      const text = new PIXI.Text(s, style)
+      text.style.dropShadow = true
+
+      const rect = new PIXI.Graphics()
+      rect.lineStyle(5, Colors.white, 1)
+      rect.addChild(text)
+
+      const random = Math.round(Math.random() * 5 + 4)
+      const value = Math.round(Math.random()) === 1 ? 0.2 : -0.2
+
+      const randomRotation = parseFloat((Math.random() * value).toFixed(9))
+
+      const width = random * 8
+      const height = 5 * 8
+      text.style.fontSize = random * 5
+      rect.position.x += lastRectX
+      lastRectX += width - 10
+      rect.rotation = randomRotation
+      text.position.x = rect.width / 2.5
+
+      return {
+        red: (): PIXI.Graphics => {
+          text.style.fill = Colors.white
+          rect.beginFill(Colors.red)
+          rect.drawRect(0, 0, width, height)
+          rect.endFill()
+          return rect
+        },
+        black: (): PIXI.Graphics => {
+          text.style.fill = Colors.red
+          rect.beginFill(Colors.black)
+          rect.drawRect(0, 0, width, height)
+          rect.endFill()
+          return rect
+        },
+      }
+    }
+
+    const texts = 'BiliBili 趋 势'.replace(/[ ]/g, '').split('')
+
+    return texts.map((s: string, i: number) => {
+      return Math.round(Math.random()) === 0
+        ? generatorRect(s, i).red()
+        : generatorRect(s, i).black()
+
+    })
+
+  }
+
   // private addStarAnimation(target: PIXI.Container) {
   //   const x = target.x - 15
   //   const y = target.y - 10
@@ -267,13 +222,105 @@ export class PIXIService {
   // }
 
   private createFPS() {
-    const FPS = createText("FPS:" + this.app.ticker.FPS.toFixed(2))
+    const FPS = createText("FPS:" + this.app.ticker.FPS.toFixed(2), 14)
     FPS.skew.set(-0.5, 0)
     return FPS
   }
 
   private createFPSAnimation() {
-    
+
+    const offset = 5
+
+    // 最左边的位置
+    const left = offset * 0
+    // 中间点位置
+    const middle = offset * 5
+    // 右边位置
+    const right = offset * 10
+    // 角度
+    const angle = offset
+    // 线长度
+    const len = offset * 2
+
+    const graphics = new PIXI.Graphics()
+
+    return range(0, 5, 1).map((v) => {
+      const voffset = v * offset
+
+      graphics.beginFill(v / 2 === 0 ? Colors.white : Colors.black) // Yellow
+
+      // X 中心顶点
+      const tm = {
+        x: middle + voffset,
+        y: 0 + voffset,
+      }
+      // X 中心顶点角度(中心顶点向右偏移位置)
+      const tmr = {
+        x: angle + voffset,
+        y: len + voffset,
+      }
+
+      // X 最右边的点
+      const r = {
+        x: right + voffset,
+        y: len + voffset,
+      }
+
+      // 右边下面的
+      const rb = {
+        x: middle + len * 2 + voffset,
+        y: len * 2 + voffset
+      }
+      // X 下最右边的点
+      const br = {
+        x: middle + len * 3 + voffset,
+        y: len * 3 + voffset,
+      }
+      // Y 中心底部点
+      // TODO 有问题
+      const mb = {
+        x: middle + voffset,
+        y: len * 2 + voffset,
+      }
+
+      // X 下最左边的点
+      const bl = {
+        x: len + voffset,
+        y: len * 3 + voffset,
+      }
+
+      // 左边下面的
+      const lb = {
+        x: middle - len * 2 + voffset,
+        y: len * 2 + voffset
+      }
+
+      // X 最左边的点
+      const l = {
+        x: 0 + voffset,
+        y: len + voffset
+      }
+      // X 中心顶点角度(中心顶点向左偏移位置)
+      const tml = {
+        x: middle - angle + voffset,
+        y: len + voffset,
+      }
+
+      graphics.drawPolygon([
+        tm.x, tm.y,
+        tmr.x, tmr.y,
+        r.x, r.y,
+        rb.x, rb.y,
+        br.x, br.y,
+        mb.x, mb.y,
+        bl.x, bl.y,
+        lb.x, lb.y,
+        l.x, l.y,
+        tml.x, tml.y,
+      ])
+      graphics.endFill()
+      return graphics
+    })
   }
 
 }
